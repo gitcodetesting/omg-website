09aa8b07740b48efd61f744b561c46620b824dd1
moonlambocap <82017259+moonlambocap@users.noreply.github.com>
2022-05-04 11:29:15 +0200

    Update about.js

--- components/about.js
+++ components/about.js
@@ -16,10 +16,13 @@ export default function About() {
       <img className="absolute top-0 right-0 bottom-0 left-0 -z-10 w-full h-full object-cover" src="/video/about.gif" autoPlay muted loop></img>
       <div className="absolute top-60 right-0 left-0">
         <div className="text-center text-white md:text-2xl max-w-2xl mx-auto font-poppins px-5">
-          <p className="mb-5">Occupy Mars Games is near future gaming.</p>
-          <p className="mb-5">You're a gamer in 2022 on Earth. Play-to-earn is in its infancy.</p>
-          <p className="mb-5">This is not right,.. 'Pay To Play' is the past,</p>
-          <p>Welcome to the future of rewarding gameplay</p>
+          <p className="mb-5">Gamers of Earth, Mars is calling you!</p>
+          <p className="mb-5">You find yourself in the near future, Starships have brought thousands of people to Mars, the first wave of exploration and pioneers is over, we are now world building.</p>
+          <p className="mb-5">The United Republics of Mars is an Earth based corporation and the the only quasi official government body on Mars. All off planet transport is contracted to the URM. It controls imports to the planet and maintains fragile relations with the guilds. The URM have military rockets to enforce its monopoly on inter-planetary transport.</p>
+          <p className="mb-5">Play the game, forge the future of Mars and you shall be rewarded with:</p>
+          <p className="mb-5">Dogecoin,</p>
+          <p className="mb-5">Land,</p>
+          <p className="mb-5">Citizenship.</p>
         </div>
       </div>
     </div>
