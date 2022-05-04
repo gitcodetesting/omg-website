65796b5e0e7ed83b0228cd3bbd5ffd63cc6b8b14
moonlambocap <82017259+moonlambocap@users.noreply.github.com>
2022-05-04 15:37:53 +0200

    Update about.js

--- components/about.js
+++ components/about.js
@@ -17,8 +17,8 @@ export default function About() {
       <div className="absolute top-40 right-0 left-0">
         <div className="text-center text-white md:text-2xl max-w-2xl mx-auto font-poppins px-5">
           <p className="mb-5">Gamers of Earth, Mars is calling you!</p>
-          <p className="mb-5">You find yourself in the near future. Starships have brought thousands of people to Mars. Now civilisation takes its first steps into a Martian future, The new generation of Martians now yearns for independence from Earth.</p>
-          <p className="mb-5">United Republics of Mars is an Earth based corporation and the the only quasi official government body on Mars with a monopoly on inter-planetary transport.</p>
+          <p className="mb-5">You find yourself in the near future. Starships have brought thousands of people to Mars. Now civilisation takes its first steps into a Martian future. A new generation of Martians now yearns for independence from Earth.</p>
+          <p className="mb-5">United Republics of Mars is an Earth based corporation and the only quasi official government body on Mars with a monopoly on inter-planetary transport.</p>
           <p className="mb-5">Play the game, forge the future of Mars and you shall be rewarded with:</p>
           <p className="mb-5"></p>
           <p className="mb-5">Dogecoin, Land, Citizenship.</p>
