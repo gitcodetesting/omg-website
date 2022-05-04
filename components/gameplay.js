7a3b8914b8b711e4cf13e07a069db057e3140d7e
moonlambocap <82017259+moonlambocap@users.noreply.github.com>
2022-05-04 16:42:45 +0200

    doge

--- components/gameplay.js
+++ components/gameplay.js
@@ -18,8 +18,8 @@ export default function GamePlay() {
         <Link href="#">
           <a>
             <div className="relative">
-              <div className="bg-black opacity-60" style={{width: 400, height: 130, borderRadius: '50%'}}></div>
-              <img src="/logo-sm.png" width={250} height={96} style={{position: 'absolute', top: 12, left: 69}} />
+              <! <div className="bg-black opacity-60" style={{width: 400, height: 130, borderRadius: '50%'}}></div> -->
+              <img src="/doge.png" width={250} height={96} style={{position: 'absolute', top: 12, left: 69}} />
             </div>
           </a>
         </Link>
